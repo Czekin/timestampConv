6fc30d6ec539010fff464e8c3562e400cb2b7947
MadMatix <madmatix.pl@gmail.com>
2020-12-16 02:35:36 +0100

    test file

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const Converter = require('./src/index');
 const date = new Converter.date(1608076029);
 const timestamp = new Converter.timestamp('Tue, 15 Dec 2020 23:30:24 GMT');
-const discordTimestamp = new Converter.discordTimestamp();
+const discordTimestamp = new Converter.discordTimestamp('2020-07-03T14:28:13.955Z');
 
 console.log(date.getDayOfWeek(), date.getDay(), date.getMonth(), date.getYear(),
     date.getHour(), date.getMinute(), date.getSeconds(), date.getMilliseconds());
@@ -9,5 +9,9 @@ console.log(date.getDayOfWeek(), date.getDay(), date.getMonth(), date.getYear(),
 console.log(timestamp.getDayOfWeek(), timestamp.getDay(), timestamp.getMonth(), timestamp.getYear(),
     timestamp.getHour(), timestamp.getMinute(), timestamp.getSeconds(), timestamp.getMilliseconds());
 
+console.log(discordTimestamp.getDayOfWeek(), discordTimestamp.getDay(), discordTimestamp.getMonth(), discordTimestamp.getYear(),
+    discordTimestamp.getHour(), discordTimestamp.getMinute(), discordTimestamp.getSeconds(), discordTimestamp.getMilliseconds());
+
 console.log(date.formatDay, date.formatHour, date.formatSeconds);
 console.log(timestamp.formatDay, timestamp.formatHour, timestamp.formatSeconds);
+console.log(discordTimestamp.formatDay, discordTimestamp.formatHour, discordTimestamp.formatSeconds);
